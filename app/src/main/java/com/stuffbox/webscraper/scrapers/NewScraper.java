f14e989b5bc07abd26f6d8c300e61aa3ece2933e
balvinderz <balvindersi2@gmail.com>
2021-06-19 11:31:09 +0530

    fix scraper

--- app/src/main/java/com/stuffbox/webscraper/scrapers/NewScraper.java
+++ app/src/main/java/com/stuffbox/webscraper/scrapers/NewScraper.java
@@ -5,11 +5,15 @@ import android.util.Log;
 import com.stuffbox.webscraper.models.Quality;
 
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NewScraper extends  Scraper{
     private Document gogoAnimePageDocument ;
@@ -23,14 +27,28 @@ public class NewScraper extends  Scraper{
             Log.i("newScraperRunning","running");
         String vidStreamUrl = gogoAnimePageDocument.getElementsByClass("play-video").get(0).getElementsByTag("iframe").get(0).attr("src");
         Log.i("vidsteramurl is",vidStreamUrl);
-        vidStreamUrl = vidStreamUrl.replaceAll("streaming.php","ajax.php");
+        ArrayList<Quality> qualities  = new ArrayList<>();
+
+        vidStreamUrl = vidStreamUrl.replaceAll("streaming.php","loadserver.php");
         try {
             Document page = Jsoup.connect(vidStreamUrl).ignoreContentType(true).get();
-            JSONObject jsonObject = new JSONObject(page.text());
-            String qualityUrl = ((JSONObject)jsonObject.getJSONArray("source").get(0)).getString("file");
-            String quality = "HD P";
-            ArrayList<Quality> qualities  = new ArrayList<>();
-            qualities.add(new Quality(quality,qualityUrl));
+//            JSONObject jsonObject = new JSONObject(page.text());
+            for(Element  element: page.getElementsByTag("script")) {
+                if(element.outerHtml().contains("playerInstance.setup"))
+                {
+                    Pattern pattern = Pattern.compile("\\[\\.*.*");
+                    Matcher matcher = pattern.matcher(element.outerHtml());
+                    if (matcher.find())
+                    {
+                        Log.i("matched text is",element.outerHtml().substring(matcher.start(),matcher.end()));
+                        String text = element.outerHtml().substring(matcher.start(),matcher.end()).replace("file","'file'").replace("label","'label'");
+                        JSONArray array = new JSONArray(text);
+                        String url = array.getJSONObject(0).getString("file");
+                        String label = array.getJSONObject(0).getString("label");
+                        qualities.add(new Quality(label,url));
+                    }
+                }
+            }
             return qualities;
 
         } catch (Exception e) {
